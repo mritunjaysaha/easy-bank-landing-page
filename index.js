ef15b4e5c641142fb85f599ed17eb580bc38da73
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-07-28 14:36:33 +0530

    added menu

---
+++ index.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close__btn");
+const menu = document.querySelector("#mobile__menu");
+btn.addEventListener("click", () => {
+    console.log("clicked");
+    btn.classList.add("disable");
+    closeBtn.classList.remove("disable");
+    menu.classList.remove("disable");
+});
+
+closeBtn.addEventListener("click", () => {
+    btn.classList.remove("disable");
+    closeBtn.classList.add("disable");
+    menu.classList.add("disable");
+});
